49b4fca82e860ea913964702f2cfd65abf207fb9
baturay47 <baturayustundag@gmail.com>
2018-10-28 02:36:15 +0300

    Added Main Menu (non-functional)
    Added a main menu which you can choose the difficulty by selecting ball size, ball speed, pad size and pad speed.
    The menu is not interacting with the game yet, will add the functionality soon.

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour {
+
+    public Button playButton;
+
+    public Slider ballsizeSlider;
+    public Text ballsizeText;
+
+    public Slider ballspeedSlider;
+    public Text ballspeedText;
+
+    public Slider padsizeSlider;
+    public Text padsizeText;
+
+    public Slider padspeedSlider;
+    public Text padspeedText;
+
+    // Use this for initialization
+    void Start () {
+        playButton.onClick.AddListener(StartGame);
+
+        ballsizeSlider.onValueChanged.AddListener(delegate { BallSizeChecker(); });
+        ballspeedSlider.onValueChanged.AddListener(delegate { BallSpeedChecker(); });
+        padsizeSlider.onValueChanged.AddListener(delegate { PadSizeChecker(); });
+        padspeedSlider.onValueChanged.AddListener(delegate { PadSpeedChecker(); });
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    void StartGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    void BallSizeChecker()
+    {
+        ballsizeText.text = ValueMatcher((int)ballsizeSlider.value);
+    }
+
+    void BallSpeedChecker()
+    {
+        ballspeedText.text = ValueMatcher((int)ballspeedSlider.value);
+    }
+
+    void PadSizeChecker()
+    {
+        padsizeText.text = ValueMatcher((int)padsizeSlider.value);
+    }
+
+    void PadSpeedChecker()
+    {
+        padspeedText.text = ValueMatcher((int)padspeedSlider.value);
+    }
+
+    private string ValueMatcher(int value)
+    {
+        switch(value)
+        {
+            case 0:
+                return "small";
+            case 1:
+                return "medium";
+            case 2:
+                return "large";
+            default:
+                return "N/A";
+        }
+    }
+}
